74c586528fbc5e471f95ca44112ea0c9dd2229db
thalecarrijo09 <thales.carrijo@gmail.com>
2025-09-25 15:45:12 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+
+// Suas credenciais únicas do projeto no Firebase
+export const firebaseConfig = {
+  apiKey: "AIzaSyCVs5lG9cjrI8nsVrqlzCCZNp5vn4DHlkw",
+  authDomain: "copinha10anos.firebaseapp.com",
+  projectId: "copinha10anos",
+  storageBucket: "copinha10anos.firebasestorage.app",
+  messagingSenderId: "469533203742",
+  appId: "1:469533203742:web:056e8f5478f8fdc820fd0f",
+  measurementId: "G-BFDLWX86XL"
+};
